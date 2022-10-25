35bcff798eedfdf448aba811866f3004aef21a49
Klauber Maciel <klaubermrocha@gmail.com>
2022-10-25 16:29:28 -0300

    caneta e lapis inc
    a fUnção caneta esta pronto e o lapiz esta na metade

---
+++ MaterialEscola/Models/Caneta.cs
@@ -0,0 +1,136 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace Models
+{
+    public class Caneta
+    {
+        private string cor;
+        private string marca;
+        private int percentualCarga;
+        private string material;
+        private bool estourada;
+        private bool caida;
+        private bool tampada;
+
+
+        public string Cor
+        {
+            get { return cor; }
+            set { cor = value; }
+        }
+
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+
+
+        public int PercentualCarga
+        {
+            get { return percentualCarga; }
+            set { percentualCarga = value; }
+        }
+
+        public bool Caida
+        {
+            get { return caida; }
+            set { caida = value; }
+        }
+
+        public bool Tampada
+        {
+            get { return tampada; }
+            set { tampada = value; }
+        }
+
+        public string Material
+        {
+            get { return material; }
+            set { material = value; }
+        }
+
+        public bool Estourada
+        {
+            get { return estourada; }
+            set { estourada = value; }
+        }
+        public Caneta(string _cor, string _marca, string _material)
+        {
+            Cor = _cor;
+            Marca = _marca;
+            PercentualCarga = 100;
+            Material = _material;
+        }
+        public void Escrever(string _texto)
+        {
+            if (this.Tampada)
+            {
+                Console.WriteLine("Você não pode escrever com a caneta tampada.");
+                return;
+            }
+
+            if (this.Caida)
+            {
+                Console.WriteLine("Antes pegue a caneta do chão.");
+                return;
+            }
+
+            if (this.Estourada)
+            {
+                Console.WriteLine("Acaneta está estourada :(");
+                return;
+            }
+
+            Console.WriteLine(_texto);
+            GastaTrinta();
+        }
+        private void GastaTrinta()
+        {
+            PercentualCarga -= 10;
+        }
+
+
+
+        public void Destampar()
+        {
+            this.Tampada = false;
+        }
+
+        public void Tampar()
+        {
+            this.Tampada = true;
+        }
+
+        public void Cair()
+        {
+            this.Caida = true;
+        }
+
+        public void PegarDoChao()
+        {
+            this.Caida = false;
+        }
+
+        public void Estourar()
+        {
+            this.Estourada = true;
+        }
+
+
+
+        public void Exibir()
+        {
+            Console.WriteLine("Cor: " + this.Cor);
+
+            Console.WriteLine("Tampada: " + this.Tampada);
+            Console.WriteLine("PercentualCarga: " + this.PercentualCarga);
+            Console.WriteLine("Marca: " + this.Marca);
+            Console.WriteLine("Material: " + this.Material);
+
+
+            Console.WriteLine("Caida: " + this.Caida);
+            Console.WriteLine("Estourada: " + this.Estourada);
+        }
+    }
+}
---
+++ MaterialEscola/Models/Lapis.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    internal class Lapis
+    {
+        private string dureza;
+        private bool apontado;
+        private int percentualGrafite;
+            
+        public string Dureza
+        {
+            get { return dureza; }
+            set { dureza = value; }
+
+        }
+        public bool Apontado
+        {
+            get { return apontado; }
+            set { apontado = value; }
+        }
+        private  int PercentualGrafite
+            {
+            get { return percentualGrafite;}
+            set { percentualGrafite = value; }
+            }
+        
+        public Lapis(string dureza  )
+        {
+            Dureza = dureza;
+            Apontado = true;
+            PercentualGrafite = 100;
+
+        }
+        public void QuebrarPonta()
+        {
+            if (PercentualGrafite < 15) 
+        }
+    }
+}
